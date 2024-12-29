f8f35bc1c1094c71bff905c42dc2cbc40aea0904
root4kaido <nikaiduhuhu@gmail.com>
2024-12-29 03:44:00 +0000

    [Update] comment

--- train_total.sh
+++ train_total.sh
@@ -2,7 +2,7 @@
 
 # pretrain
 python3 train.py model_name=CustomLGBMRegressor exp_name=lgbm_regressor_pretrain
-python3 inference.py model_name=CustomLGBMRegressor exp_name=lgbm_regressor_pretrain model_path=/home/user/work/CMI2024/Git/src/results/lgbm_regressor_pretrain/total_models.pkl phase=train
+python3 inference.py model_name=CustomLGBMRegressor exp_name=lgbm_regressor_pretrain model_path=/home/user/work/CMI2024/Git/src/results/lgbm_regressor_pretrain/total_models.pkl phase=pseudo
 
 # train
 python3 train.py model_name=CustomLGBMRegressor exp_name=lgbm_regressor oof_path=/home/user/work/CMI2024/Git/src/results/lgbm_regressor_pretrain/pseudo_submission.csv
